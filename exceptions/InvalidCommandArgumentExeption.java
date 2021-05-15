a1d4aaa8a5c1d2198a145c97ddeef6fa64b28a75
Kim Daniel <64163182+KIMdaniiell@users.noreply.github.com>
2021-05-15 16:31:47 +0300

    Add files via upload

--- exceptions/InvalidCommandArgumentExeption.java
+++ exceptions/InvalidCommandArgumentExeption.java
@@ -3,13 +3,14 @@ package exceptions;
 /**
  * Thrown to indicate that invalid arguments are given
  */
-public class InvalidCommandArgumentExeption extends Exception{
+public class InvalidCommandArgumentExeption extends Exception {
 
     /**
      * Exceptions' default constructor
+     *
      * @param message - this message will pop out in System.out stream if an exception
      */
-    public InvalidCommandArgumentExeption(String message){
+    public InvalidCommandArgumentExeption(String message) {
         super(message);
     }
 }
--- exceptions/InvalidInputValueException.java
+++ exceptions/InvalidInputValueException.java
@@ -1,15 +1,16 @@
 package exceptions;
 
 /**
- * Exception class offers to overstepping a restrictions, which are whiten in the Task
+ * Thrown if given field value does not suit Task restrictions
  */
-public class InvalidInputValueException extends Exception{
+public class InvalidInputValueException extends Exception {
 
     /**
      * Exceptions' default constructor
+     *
      * @param message - this message will pop out in System.out stream if an exception
      */
-    public InvalidInputValueException (String message){
+    public InvalidInputValueException(String message) {
         super(message);
     }
 }
