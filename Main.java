428ffc07f037bf3e9b7530d104c78d1fb0f1157a
Kim Daniel <64163182+KIMdaniiell@users.noreply.github.com>
2021-05-15 16:31:55 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import data.Parser;
 
 import java.util.Scanner;
 import java.util.Stack;
+
 import data.format.MusicBand;
 
 /**
@@ -11,18 +12,18 @@ import data.format.MusicBand;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         String data_path = System.getenv("DPATH");
-        if (data_path==null){
+        if (data_path == null) {
             System.out.println("Переменная окружения не найдена");
             System.exit(1);
         } else {
+
+            /* Forming collection of instances of MusicBand class */
             Stack<MusicBand> collection = Parser.serialize(data_path);
 
-            /**
-             * Code need for receiving commands from terminal
-             */
+            /* Code need for receiving commands from terminal */
             Scanner sc = new Scanner(System.in);
             while (true) {                                              //while not receiving "exit" command
                 Commander.doCommand(collection, data_path, sc);
